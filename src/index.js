83d848f1f4d234e7ba9a9d2448111d67bfa58a00
Gabriel Rea <gabriel.rea96@gmail.com>
2021-03-19 17:59:01 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+// Componentes
+import GifExpertApp from './components/GifExpertApp';
+
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
+
+// GifExpertApp
+// <h2>GifExpertApp</h2>
+// <hr />
